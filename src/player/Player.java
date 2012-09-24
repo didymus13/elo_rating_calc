deb562733bbb5f2ef9ca1cb24d0bae7e15287261
Stephane Doiron <didymus1313@gmail.com>
2012-09-24 16:25:20 -0400

    Ratings are being calculated correctly

--- src/player/Player.java
+++ src/player/Player.java
@@ -72,7 +72,6 @@ public class Player {
 	}
 	
 	public double calcK(double Ne, int m) {
-		// TODO
 		return (double) 800 / (Ne + m); 
 	}
 	
@@ -102,15 +101,42 @@ public class Player {
 	}
 
 	public void applyResults(int[] r, int wNew, int lNew) {
-		if (this.getTotalGames() <= 25 ) {
-			// Calculate provisional rating
+		if (this.isEstablished() ) 
+			this.rating = 0;
+		else
 			this.rating = this.calcProvisionalRating(r, wNew, lNew);
-			this.win = this.win + wNew;
-			this.loss = this.loss + lNew;
-			this.draw = this.draw + (r.length - wNew - lNew);
-		} else {
-			// Calculate Established rating
+		this.win = this.win + wNew;
+		this.loss = this.loss + lNew;
+		this.draw = this.draw + (r.length - wNew - lNew);
+	}
+	
+	public boolean isEstablished() {
+		return (this.getTotalGames() > 25) ? true : false;
+	}
+
+	public double calcEffectiveGames() {
+		double nr;
+		if (this.rating >= 2200 || this.getTotalGames() > 50) 
+			nr = 50;
+		else
+			nr = 50 / Math.sqrt((1+Math.pow(2200 - this.rating, 2)/100000));
+		return (double) Math.min(this.getTotalGames(), nr);
+	}
+	
+	public int calcEffectiveRating(int[] r, int wNew, int lNew) {
+		double k = this.calcK(this.calcEffectiveGames(), r.length);
+		double s = wNew + Math.abs(wNew - lNew)*0.5;
+		double e = this.calcWinExpectancy(r);
+		double newRating = Math.round(this.rating + k*(s-e));
+		return (int) newRating;
+	}
+	
+	public double calcWinExpectancy(int[] opponents) {
+		double we = 0;
+		for (int i = 0; i < opponents.length; i++) {
+			we = we + (1 / (Math.pow((double) 1/10, (double) (this.rating - opponents[i])/400 ) +1) );
 		}
+		return we;
 	}
 
 }
--- src/player/PlayerTest.java
+++ src/player/PlayerTest.java
@@ -4,6 +4,8 @@
 package player;
 
 
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 import org.junit.*;
@@ -14,11 +16,20 @@ import org.junit.*;
  */
 public class PlayerTest extends TestCase{
 
+	Player player = new Player("Mr Test");
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		int[] opponents = new int[26];
+		int i;
+		for (i=0; i <= 25; i++) {
+			opponents[i] = 1000;
+		}
+		
+		this.player.applyResults(opponents, 13, 13);
 	}
 
 	/**
@@ -48,6 +59,26 @@ public class PlayerTest extends TestCase{
 		newPlayer.applyResults(opponents2, 1, 0);
 		this.assertEquals(1130, newPlayer.getRating());
 		this.assertEquals(5, newPlayer.getTotalGames());
+	}
+	
+	@Test
+	public void testCalcEffectiveGames() {
+		this.assertEquals(12.741179785940638, this.player.calcEffectiveGames());	
+	}
+	
+	@Test
+	public void testCalcK() {
+		this.assertEquals(58.21916403557461, this.player.calcK(this.player.calcEffectiveGames(), 1));
+	}
+	
+	@Test
+	public void testEstablishedRating() {
+		this.assertTrue(this.player.isEstablished());
+		int[] opponents = {1000, 1000};
+		int win = 1;
+		int loss = 1;
+		this.assertEquals(1.0, this.player.calcWinExpectancy(opponents));
+		this.assertEquals(1000, this.player.calcEffectiveRating(opponents, win, loss));
 	}
 
 }
