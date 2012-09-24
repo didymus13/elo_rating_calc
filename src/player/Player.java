d4248b01cfa18cd4a0444632efbdc60d972edf26
Stephane Doiron <didymus1313@gmail.com>
2012-09-24 10:24:58 -0400

    basic files created

---
+++ src/player/Player.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package player;
+
+/**
+ * @author maddox
+ *
+ */
+public class Player {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
